0268f4a5849f652cfc6aa3d803069ceb8ec99ec3
Rodel Makilan <c20234112.makilan@csav.edu.ph>
2025-04-25 09:53:17 +0800

    Create navbar.php

---
+++ include/navbar.php
@@ -0,0 +1,22 @@
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="std_account.php">Homepage</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="std_account.php">Account</a>
+         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="users.php">Classroom</a>
+         </li>
+         </ul>
+      
+    </div>
+  </div>
+</nav>
